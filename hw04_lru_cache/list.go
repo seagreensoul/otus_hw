e12c4795508765df8896877430ecb4b41b55eed7
n.nesterik <n.nesterik@rambler-co.ru>
2023-08-27 18:03:54 +0300

    hw04 first part of hw

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,8 +17,70 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	len       int
+	FirstItem *ListItem
+	LastItem  *ListItem
+}
+
+func (dl list) Len() int {
+	return dl.len
+}
+
+func (dl list) Front() *ListItem {
+	if dl.len == 0 {
+		return nil
+	}
+
+	return dl.FirstItem
+}
+
+func (dl list) Back() *ListItem {
+	if dl.len == 0 {
+		return nil
+	}
+
+	return dl.LastItem
+}
+
+func (dl *list) PushFront(v interface{}) *ListItem {
+	if dl.len == 0 {
+		li := ListItem{v, nil, nil}
+		dl.LastItem = &li
+		dl.FirstItem = &li
+	} else {
+		li := ListItem{v, dl.FirstItem, nil}
+		dl.FirstItem.Prev = &li
+		dl.FirstItem = &li
+	}
+	dl.len++
+
+	return dl.FirstItem
+}
+
+func (dl *list) PushBack(v interface{}) *ListItem {
+	if dl.len == 0 {
+		li := ListItem{v, nil, nil}
+		dl.LastItem = &li
+		dl.FirstItem = &li
+	} else {
+		li := ListItem{v, nil, dl.LastItem}
+		dl.LastItem.Next = &li
+		dl.LastItem = &li
+	}
+	dl.len++
+
+	return dl.LastItem
+}
+
+func (dl *list) Remove(i *ListItem) {
+	i.Next.Prev = i.Prev
+	i.Prev.Next = i.Next
+	dl.len--
+}
+
+func (dl *list) MoveToFront(i *ListItem) {
+	dl.Remove(i)
+	dl.PushFront(i.Value)
 }
 
 func NewList() List {
